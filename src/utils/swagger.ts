ee61edab41337c636c1ead67a9c168dd15763f3c
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2020-03-19 09:51:52 +0000

    swagger.ts remove http from SERVER_HOST

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -16,7 +16,7 @@ export const swaggerUi = setup(swaggerJSDoc({
             version: packageJson.version,
             description: "API documentation for GSPoste"
         },
-        host: `${process.env.SERVER_HOST || "http://localhost"}:${
+        host: `${process.env.SERVER_HOST.replace("http://", "")}:${
             process.env.NODE_ENV !== "production" ? (process.env.SERVER_PORT || "5000") : ""
         }`,
         basePath: process.env.API_PATH
