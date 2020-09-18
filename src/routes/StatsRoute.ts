ae3ffd3b336ccb383eea81f2d8b8dafad58608a7
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2020-09-18 16:33:21 +0200

    Change path for user stats to /stats/user/{id}/{year}

--- src/routes/StatsRoute.ts
+++ src/routes/StatsRoute.ts
@@ -11,6 +11,45 @@ export class StatsRoute extends Route {
             /**
              * @swagger
              *
+             * /stats/user/{id}/{year}:
+             *   get:
+             *     tags:
+             *       - Stats
+             *     description: Fetch stats for the user id passed on path. If admin, you can get stats for other users.
+             *     produces:
+             *       - application/json
+             *     security:
+             *       - JWT: []
+             *     parameters:
+             *       - name: id
+             *         required: true
+             *         in: path
+             *         description: Mongo id of the user to get stats for
+             *       - name: year
+             *         required: false
+             *         in: path
+             *         description: Optional year to get the stats for. If omitted, it considers ALL the letters sent by this user
+             *     responses:
+             *       200:
+             *         description: Stats
+             *         schema:
+             *           $ref: "#/definitions/Stats"
+             *       400:
+             *         $ref: "#/responses/BadRequest"
+             *       401:
+             *         $ref: "#/responses/Unauthorized"
+             *       403:
+             *         description: You are not allowed to get statistics of other users!
+             */
+            {
+                path: "/user/:id/:year",
+                method: RequestMethod.GET,
+                requiresAuth: true,
+                handler: (req, res) => this.statsController.fetchStatsForUser(req, res)
+            },
+            /**
+             * @swagger
+             *
              * /stats/system/{year}:
              *   get:
              *     tags:
@@ -42,45 +81,6 @@ export class StatsRoute extends Route {
                 method: RequestMethod.GET,
                 requiresAuth: true,
                 handler: (req, res) => this.statsController.fetchSystemSpentStats(req, res)
-            },
-            /**
-             * @swagger
-             *
-             * /stats/{id}/{year}:
-             *   get:
-             *     tags:
-             *       - Stats
-             *     description: Fetch stats for the user id passed on path. If admin, you can get stats for other users.
-             *     produces:
-             *       - application/json
-             *     security:
-             *       - JWT: []
-             *     parameters:
-             *       - name: id
-             *         required: true
-             *         in: path
-             *         description: Mongo id of the user to get stats for
-             *       - name: year
-             *         required: false
-             *         in: path
-             *         description: Optional year to get the stats for. If omitted, it considers ALL the letters sent by this user
-             *     responses:
-             *       200:
-             *         description: Stats
-             *         schema:
-             *           $ref: "#/definitions/Stats"
-             *       400:
-             *         $ref: "#/responses/BadRequest"
-             *       401:
-             *         $ref: "#/responses/Unauthorized"
-             *       403:
-             *         description: You are not allowed to get statistics of other users!
-             */
-            {
-                path: "/:id/:year",
-                method: RequestMethod.GET,
-                requiresAuth: true,
-                handler: (req, res) => this.statsController.fetchStatsForUser(req, res)
             },
         ]);
     }
