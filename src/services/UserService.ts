b85db03303a1c5e5126f15eab517c5d52b96466c
Giovanni Orciuolo <giovanni@orciuolo.it>
2021-03-22 18:17:15 +0100

    Create first sender on user registration

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,14 +3,42 @@ import { MongoRepository } from "./MongoRepository";
 import { UserModel, User, UserDocument, UserPasswordUpdate, userDecoder } from "@models/UserModel";
 import { comparePasswords } from "@utils/crypto";
 import httpErrors from "http-errors";
+import { inject } from "inversify";
+import { SenderService } from "@services/SenderService";
 
 @provide(UserService)
 export class UserService extends MongoRepository<User, UserDocument> {
+
+    @inject(SenderService) private senderService: SenderService;
 
     constructor(private userModel = UserModel) {
         super(userModel, userDecoder, [
             "username", "email", "iva"
         ]);
+    }
+
+    public async save(user: User): Promise<UserDocument> {
+        const userDoc = await super.save(user);
+
+        // Create the first sender with empty details
+        // Will be filled later by the operator on user activation
+        await this.senderService.save({
+            user: userDoc.id,
+            name: user.username,
+            description: user.username,
+            email: user.email,
+            businessName: user.username,
+            invoiceCode: "0000000",
+            iva: user.iva,
+            address: {
+                street: "",
+                city: "",
+                zip: "00000",
+                province: "00"
+            }
+        });
+
+        return userDoc;
     }
 
     public async updatePassword(user: UserDocument, passwordUpdate: UserPasswordUpdate) {
