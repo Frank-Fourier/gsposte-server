2876e7e562a2bc5c37c94b3de3db4053e0449288
carminegreen <tec.carmineverde@gmail.com>
2022-10-27 13:24:20 +0200

    (fix): Fix script with invoice id

--- scripts/fixInvoiceNumber.js
+++ scripts/fixInvoiceNumber.js
@@ -1,5 +1,6 @@
 // noinspection NpmUsedModulesInstalled
-const { MongoClient, ObjectID } = require("mongodb");
+const mongodb = require("mongodb");
+const { MongoClient } = require("mongodb");
 const data = require('./data.json');
 
 const DB_URI = "mongodb://localhost:27017";
@@ -16,7 +17,7 @@ const DB_NAME = "gsposte_prod";
         const invoices = db.collection("invoices");
 
         for (const obj of data) {
-            await invoices.updateOne({ _id: ObjectID.createFromHexString(obj.id) }, {
+            await invoices.updateOne({ _id: mongodb.ObjectID(obj.id) }, {
                 $set: {
                     number: +obj.new,
                     createdAt: new Date("2022-10-20T09:33:40.051Z"),
