c4d22585790cbcf5caea2d5ec6f8e9a96f2a8434
carminegreen <tec.carmineverde@gmail.com>
2022-10-27 12:02:56 +0200

    (feat): Add fix invoice number script

---
+++ scripts/fixInvoiceNumber.js
@@ -0,0 +1,32 @@
+// noinspection NpmUsedModulesInstalled
+const { MongoClient } = require("mongodb");
+const data = require('./data.json');
+
+const DB_URI = "mongodb://localhost:27017";
+const DB_NAME = "gsposte_prod";
+
+(async function() {
+    const client = new MongoClient(DB_URI, { useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const db = client.db(DB_NAME);
+        console.log("Connected to database @ " + DB_URI);
+
+        const invoices = db.collection("invoices");
+
+        for (const obj of data) {
+            await invoices.updateOne({ number: +obj.prev }, {
+                $set: {
+                    number: +obj.new,
+                    createdAt: new Date("2022-10-20T09:33:40.051Z"),
+                    updatedAt: new Date("2022-10-20T09:33:40.051Z")
+            } },{ w: 1 });
+            process.stdout.write("Done!\n");
+        }
+
+    } finally {
+        console.log("Closing connection with database");
+        await client.close();
+    }
+})().catch(console.dir);
